3586882289d536489c2512bceab20789230c30e9
kenjoroge <njorogeken57@gmail.com>
2023-09-26 06:39:59 -0700

    Changed

--- main.h
+++ main.h
@@ -43,7 +43,7 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-// printiing characters and strings
+/*printiing characters and strings*/
 
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
